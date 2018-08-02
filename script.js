96160815aae6c39e3e0a46ec71d8161d5619cdd7
Lorniebee <lorniebrown@hotmail.com>
2018-08-02 22:38:54 +0200

    added side nav & css & js files

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenavClosed").style.width = "0px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenavClosed").style.width = "50px";
+}
